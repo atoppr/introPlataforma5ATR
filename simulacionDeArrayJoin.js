89e2ecf9aba17fb967664c41803889124f23ac0e
atoppr <66582383+atoppr@users.noreply.github.com>
2022-07-24 12:39:14 -0300

    Update simulacionDeArrayJoin.js

--- simulacionDeArrayJoin.js
+++ simulacionDeArrayJoin.js
@@ -1,9 +1,4 @@
-/* En este ejercicio deberás crear una Función llamada join que reciba un Arreglo y simule el comportamiento del método Array.join().
-⚠️Importante: No podés usar el método Array.join() original.
-Por ejemplo:
-join(["h","o","l","a"]) debe retornar el string "hola".
-join(["c","h","a,"u"]) debe retornar el string "chau". */
-
+/* En este ejercicio deberás crear una Función llamada join que reciba un Arreglo y simule el comportamiento del método Array.join().*/
 /*** Resolución ***/
 
 function join (arr){
@@ -14,5 +9,5 @@ function join (arr){
     return arrNew
 }
 
-join(["c","h","a","u"])  //Para testear: debe retornar el string "hola"
+join(["h","o","l","a"])  //Para testear: debe retornar el string "hola"
 join(["c","h","a,"u"]) //Para testear: debe retornar el string "chau"
